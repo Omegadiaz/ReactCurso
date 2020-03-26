cc3b417438412650a96244e32def671b1fcac8a2
Omegadiaz <omsystem.gabriel@gmail.com>
2020-03-26 23:12:38 +0000

    Pseudo selectors with Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium , { StyleRoot } from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -91,6 +91,7 @@ class App extends Component {
 
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1> Hi, I´m a React App </h1>
         <p className={classes.join(' ')}> This is really working!</p>
@@ -99,6 +100,7 @@ class App extends Component {
         
 
       </div>
+      </StyleRoot>
     );
   }
 }
